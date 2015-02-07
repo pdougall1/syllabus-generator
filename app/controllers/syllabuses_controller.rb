321a4fdf747ebce67b431c5b1710979d12c79c74
Patrick Dougall <pdougall1@gmail.com>
2015-02-07 17:40:19 -0500

    Adds create action to syllabus controller

--- app/controllers/syllabuses_controller.rb
+++ app/controllers/syllabuses_controller.rb
@@ -4,4 +4,16 @@ class SyllabusesController < ApplicationController
     @syllabus = Syllabus.new
     @syllabus.instructor = Instructor.new
   end
+
+  def create
+    @syllabus = Syllabus.create(syllabus_params)
+    redirect_to new_schedule_path(syllabus_id: @syllabus.id)
+  end
+
+  private
+
+  def syllabus_params
+    params.require(:syllabus).permit!
+  end
+
 end
