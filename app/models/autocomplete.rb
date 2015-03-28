92829c4bd211202d99638328d906c365f516db7e
Patrick <pdougall1@gmail.com>
2015-03-27 20:07:12 -0400

    Fixes autocomplete

--- app/models/autocomplete.rb
+++ app/models/autocomplete.rb
@@ -2,6 +2,10 @@ class Autocomplete
 
   def self.terms_for(substring, model)
     model = model.capitalize.constantize
-    model.where("name ilike ?", "%#{substring}%").limit(10).pluck(:name)
+    model.where("name ilike ?", "%#{substring}%")
+      .select(:name)
+      .distinct
+      .limit(10)
+      .pluck(:name)
   end
 end
